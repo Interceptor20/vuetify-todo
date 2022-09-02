ff084c038550843405cf8abde45d88badc18f07c
Interceptor20 <mikejoshua761@gmail.com>
2022-09-02 11:19:22 +0100

    Initial commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laborum ut eum, ex, dicta enim quis amet sapiente itaque voluptatum mollitia explicabo, temporibus blanditiis accusamus est. Eligendi culpa officiis voluptates!
+    </p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quidem atque eum nemo quaerat itaque tempore exercitationem nostrum officia magnam! Recusandae consectetur quaerat debitis sequi consequuntur labore porro reprehenderit. Aliquam.<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi iusto aut consectetur voluptates aliquid nemo provident sapiente, incidunt, obcaecati debitis quo repellat adipisci cupiditate magnam tenetur blanditiis soluta vitae.</p>
   </div>
 </template>
