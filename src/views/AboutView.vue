f338c0d8258f1ab6763a6b333bd5d80874efd2f6
Interceptor20 <mikejoshua761@gmail.com>
2022-09-02 12:51:59 +0100

    about contents

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about pa-6">
     <h1>About Vuetify Todo</h1>
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae laborum ut eum, ex, dicta enim quis amet sapiente itaque voluptatum mollitia explicabo, temporibus blanditiis accusamus est. Eligendi culpa officiis voluptates!
-    </p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis quidem atque eum nemo quaerat itaque tempore exercitationem nostrum officia magnam! Recusandae consectetur quaerat debitis sequi consequuntur labore porro reprehenderit. Aliquam.<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad eligendi iusto aut consectetur voluptates aliquid nemo provident sapiente, incidunt, obcaecati debitis quo repellat adipisci cupiditate magnam tenetur blanditiis soluta vitae.</p>
+    <p>ICONS ON THE TOP RIGHT CORNER UNDER CONSTRUCTION.
+      TODO LIST: type it. read it. do it. type it. read it. do it. type it. read it. do it. type it. read it. do it.
+    </p>Edit your list. cross completed tasks.Edit your list. cross completed tasks.Edit your list. cross completed tasks.Edit your list. cross completed tasks.Edit your list. cross completed tasks.<p>Make a List. Get things done. Make a list. get things done. make a list. get things done. make a list. get things done. make a list. get things done. make a list. get things done.</p>
   </div>
 </template>
